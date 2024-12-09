a278ab361c050334a4fa6ef5bbae2443f3994066
Filip Růžička <f@filipruzicka.com>
2024-12-09 17:16:31 +0100

    Add GET/POST endpoints

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,19 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+		if (request.method === 'GET') {
+			const value = await env.PALMA_HAYEK_KV.get('MOISTURE');
+			if (value === null) {
+				return new Response('Value not found', { status: 404 });
+			}
+			return new Response(value);
+		}
+
+		if (request.method === 'POST') {
+			const value = await request.text();
+			await env.PALMA_HAYEK_KV.put('MOISTURE', String(value));
+			return new Response('Value set');
+		}
+
+		return new Response('Method not allowed', { status: 405 });
 	},
 } satisfies ExportedHandler<Env>;
